1c0e74ff8f0d7f4b9815627c3a484d0c32355d78
ekamleen <ekamleenm@gmail.com>
2024-09-17 15:37:38 -0700

    ll insert_end

--- data-strucs/lists/linkedlist1.c
+++ data-strucs/lists/linkedlist1.c
@@ -1,26 +1,60 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struc Node{
+struct Node{
 	int data;
-	struct Node* next
+	struct Node* next;
+};
+
+struct Node* head;// global variable
+
+void insert_end(int x){
+
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	// this is making the first node of the linkedlist.
+	newNode->data = x;
+	newNode->next = NULL;
+	if (head == NULL){	
+		head = newNode;
+	}
+	else{
+		struct Node* temp;
+		temp = head;
+		while(temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+
+		temp->next = newNode;
+	}
 }
 
-struct Node* head; // global variable
-void insert(int x);
-void print();
+void insert_begining(int x){
+	
+}
 
-int main() 
+void print_list(){
+	struct Node* temp = head;
+	while(temp != NULL){
+		printf("%d->", temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+
+int main(){ 
 	head = NULL; // empty list 
 	int n, i, x;
-	print("Length of the linked list: ");
+	printf("Length of the linked list: ");
 	scanf("%d", &n);
 	for(i =0; i < n; i++){
 		printf("enter the number: \n");
 		scanf("%d", &x);
-		insert(x);
-		print();
+		insert_end(x);
+		print_list();		
 	}
+
+	
 }
 
 
